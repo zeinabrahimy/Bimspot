acbb99eddab9dc2dab5b537e6be33da6bd85b920
z.rahimi <z.rahimi@ZRAHIMI-PC>
2020-10-22 13:57:42 +0330

    ADD Integration Tests.

--- IntegrationTest/Species/GetAllMammal.cs
+++ IntegrationTest/Species/GetAllMammal.cs
@@ -6,5 +6,28 @@ namespace BimspotTest.Service.IntegrationTest.Species
 {
 	public class GetAllMammal
 	{
+		[Fact]
+		public async void GetAllMammal_RightScenario()
+		{
+			// Arrange
+			IConfiguration configuration = new ConfigurationBuilder()
+																				 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+																				 .AddEnvironmentVariables()
+																				 .Build();
+
+			IServiceCollection services = new ServiceCollection();
+			services.AddSingleton(configuration);
+			services.ConfigureServices();
+
+			var serviceProvider = services.BuildServiceProvider();
+
+			var speciesService = serviceProvider.GetService<BimspotTest.Service.ISpeciesService>();
+
+			// Act
+			var result = await speciesService.GetAllMammals().ConfigureAwait(false);
+
+			// Assert
+			Assert.NotNull(result);
+		}
 	}
 }
--- IntegrationTest/Species/GetCriticalEndangered.cs
+++ IntegrationTest/Species/GetCriticalEndangered.cs
@@ -6,5 +6,28 @@ namespace BimspotTest.Service.IntegrationTest.Species
 {
 	public class GetCriticalEndangered
 	{
+		[Fact]
+		public async void GetCriticalEndangered_RightScenario()
+		{
+			// Arrange
+			IConfiguration configuration = new ConfigurationBuilder()
+																				 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+																				 .AddEnvironmentVariables()
+																				 .Build();
+
+			IServiceCollection services = new ServiceCollection();
+			services.AddSingleton(configuration);
+			services.ConfigureServices();
+
+			var serviceProvider = services.BuildServiceProvider();
+
+			var speciesService = serviceProvider.GetService<BimspotTest.Service.ISpeciesService>();
+
+			// Act
+			var result = await speciesService.GetCriticalEndangered().ConfigureAwait(false);
+
+			// Assert
+			Assert.NotNull(result);
+		}
 	}
 }
